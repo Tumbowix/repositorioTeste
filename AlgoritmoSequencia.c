19be019aeaccfce69b3c8c0b8aa9ecb3bd67cf7b
Tumbowix <tumbowix@gmail.com>
2024-11-06 20:34:53 -0300

    adicionando a palavra mudou

--- AlgoritmoSequencia.c
+++ AlgoritmoSequencia.c
@@ -8,7 +8,7 @@ int main()
 
     for (int i = 1; i <= 10; i++)
     {
-        printf("\n%d, 1 2 3 4 5 6 7 8 9 10", i);
+        printf("\n%d, 1 2 3 4 5 6 7 8 9 10 mudou", i);
     }
     return 0;
 }
